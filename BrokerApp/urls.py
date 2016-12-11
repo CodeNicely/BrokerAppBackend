55b96722f189c5348670040e311f977041768f14
rishi <rishiraicom@gmail.com>
2016-12-11 18:57:56 +0530

    login done

---
+++ BrokerApp/urls.py
@@ -0,0 +1,27 @@
+"""BrokerApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from splash_screen.views import splash_scr_response
+from otp.views import send_otp
+from otp.views import verify_otp
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^splash_screen/', splash_scr_response),
+    url(r'^send_otp/', send_otp),
+    url(r'^verify_otp/', verify_otp),
+    
+]
